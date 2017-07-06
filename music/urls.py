fbc1af51b0ebed82db8b79f60de459c1f8c28c32
akzarma <akzarma@gmail.com>
2017-07-06 20:59:55 +0530

    Now, Better UI

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,25 @@
 from django.conf.urls import url
 from . import views
+app_name = 'music'
+
 
 urlpatterns = [
     #/music/
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.HomeView.as_view(), name='home'),
 
     #/music/19
-    url(r'^(?P<album_id>[0-9]+)/$', views.details, name='details'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),
+
+    # /music/album/add/
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/2/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+
+
+
+
 ]
